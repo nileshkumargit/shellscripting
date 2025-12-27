998d0d4d74b7b6d6bae1324d6791652979843ff7
nilesh <nilesh@gmail.com>
2025-12-27 20:26:17 +0000

    bracket_usage.sh

---
+++ input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+first_arg="$1"
+second_arg="$2"
+
+all_arg="$@"
+
+echo "The first argument is :$first_arg"
+echo "The second argument is :$second_arg"
+echo "All arguments is :$all_arg"
